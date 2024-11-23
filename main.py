7768a4ef48109fc0fd898c27020a04aa36e13f70
jirving1 <jrving1@gmail.com>
2024-11-23 13:29:39 -0500

    player is now drawn

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -15,11 +17,12 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
+#run source venv/bin/activate to activate venv
        
     
 
